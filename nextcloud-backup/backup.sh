404485a1e5d609802ad8314be910d65d66df2a1d
Алексей Тихонов <tikhonovaa3@mos.ru>
2018-12-11 19:29:14 +0300

    проверил скрипт на запускаемость, case, for, source

--- nextcloud-backup/backup.sh
+++ nextcloud-backup/backup.sh
@@ -1,56 +1,59 @@
-#!/bin/sh
+#!/bin/bash
 
 ###  Variables:
 ## Local variables:
 ## Path to backup folder:
-date=`date +%Y-%m-%d` 
+date=`date +%Y-%m-%d`
+time=`date +%H:%M:%S` 
 out_path="/backup/nextcloud/$date"
 home_dir="/home/centos/nextcloud"
 passwords="./nextcloud.password"
 ## Docker variables:
-## Application docker container:
-#   Tag of application docker container:
-app_tag="nextcloud"
-app_image="wonderfall/nextcloud"
-#   Path to source dir in app_docker:
-user_archive=$home_dir/data/data
+images=(wonderfall/nextcloud,mariadb:10) 		# List of nextcloud images
+user_archive=$home_dir/data/data 			# Path to source dir in app_docker
+IFS=$','
 
 ## Database docker container:
-#   Tag of DB docker container:
-db_tag="db_nextcloud"
-db_image="mysql/mysql-server"
-#   DB name in DB docker container:
-db_name="nextcloud"
+db_container="db_nextcloud" 				# Name of DB docker container:
+db_name="nextcloud" 					# DB name in DB docker container
 
 #   Username and password of DBA in db_docker:
 . $passwords
-
-## Backup sequence:
+# Backup sequence:
 
 # Preps
 #   
 # Backup of app_docker
 # 1. Prep backup_dir
 date 
-mkdir $outpath
+mkdir -p $out_path
 # 2. Turn maintinance mode on
-docker exec $app_tag php /nextcloud/occ maintenance:mode --on 
+echo "$time - Turn maintinance mode on"
+#docker exec $app_tag php /nextcloud/occ \
+#maintenance:mode --on 
+
 case $1 in
-	images)
-# 3. Make backup of application docker image 
-docker save $app_image |pbzip2 -c -p4 > $outpath/$app_image-$date.tar.bz2
-# 4, Make backup of database docker image
-docker save $db_image |pbzip2 -c -p4 > $outpath/$db_image-$date.tar.bz2;;
-	db)
-# 5. Make backup of database from docker container
-;;
-	user_data)
-# 6. Make backup of user_data directory
-;;
-	*)
-echo ""
+  images) 					# Make backup of docker images:
+echo "Saving images"
+    for i in $images 
+    do
+    echo "$time - Saving $i image" 
+#    docker save $i |pbzip2 -c -p4 > $outpath/$i-$date.tar.bz2 
+    done
+    ;;
+  db)						# Make backup of database from docker container
+echo "$time - DB Backup"
+#docker exec $db_container /usr/bin/mysqldump -u $dba_user --password=$dba_pass nextcloud > backup.sql
+    ;;
+  user_data)					# Make backup of user_data directory
+echo "backup user data directory"
+    ;;
+  *)
+echo "Choose from 'images', 'db' and 'user_data'"
+    ;;
 esac
 # 7. Turn maintinance mode off
-docker exec $app_tag php /nextcloud/occ maintenance:mode --off
-echo "Backup process is DONE"
+echo "$time - Turn maintinance mode off"
+#docker exec $app_tag php /nextcloud/occ maintenance:mode --off
+echo "$time - Backup process is DONE"
 
