b8ebc46a22b67bdbbde9a8de58cda6ff54d2c9ed
Алексей Тихонов <tikhonovaa3@mos.ru>
2019-01-10 22:44:54 +0300

    Доделал скрипт, проверил 3 режима (образы, БД, Архив файлов)

--- nextcloud-backup/backup.sh
+++ nextcloud-backup/backup.sh
@@ -15,10 +15,7 @@ IFS=$','
 app_container="nextcloud"
 ## Database docker container:
 db_container="db_nextcloud" 				# Name of DB docker container:
-db_name="nextcloud" 					# DB name in DB docker container
 
-#   Username and password of DBA in db_docker:
-. $passwords
 # Backup sequence:
 
 # Preps
@@ -28,30 +25,36 @@ db_name="nextcloud" 					# DB name in DB docker container
 date 
 mkdir -p $out_path
 # 2. Turn maintinance mode on
-echo "$time - Turn maintinance mode on"
-docker exec $app_container php /nextcloud/occ maintenance:mode --on 
+#echo "$time - Turn maintinance mode on"
+#docker exec $app_container php /nextcloud/occ maintenance:mode --on 
 case $1 in
   images) 					# Make backup of docker images:
 echo "$time - Saving images"
     for i in $images 
     do
-    echo "$time - Saving $i image" 
-    docker save $i |pbzip2 -c -p4 > $outpath/$i-$date.tar.bz2 
+    filename=$(echo $i|sed 's/[:/]/_/g')
+    backup_dest=$out_path/$filename_image.tar.bz2
+    echo "$time - Saving $i image to $backup_dest"
+    docker save $i |pbzip2 -c -p4 > $backup_dest
     done
     ;;
   db)						# Make backup of database from docker container
-echo "$time - DB Backup"
-#docker exec $db_container /usr/bin/mysqldump -u $dba_user --password=$dba_pass nextcloud > backup.sql
+    echo "$time - DB Backup start"
+    echo "creating backup to $out_path/nextcloud.sql"
+    docker exec $db_container sh -c 'exec /usr/bin/mysqldump -uroot -p"$MYSQL_ROOT_PASSWORD" "$MYSQL_DATABASE"' > $out_path/nextcloud.sql
     ;;
   user_data)					# Make backup of user_data directory
-echo "backup user data directory"
+    echo "$time - Backup user data directory from '$user_archive/' to '$out_path/user_archive.tar.bz2'"
+#    tar cf $out_path/user_archive-$date.tar.bz2 --use-compress-prog=pbzip2 $user_archive/
+    tar -c $user_archive/| pbzip2 -c -p4 > $out_path/user_archive.tar.bz2
     ;;
   *)
 echo "Choose from 'images', 'db' and 'user_data'"
     ;;
 esac
 # 7. Turn maintinance mode off
-echo "$time - Turn maintinance mode off"
-docker exec $app_container php /nextcloud/occ maintenance:mode --off
+#echo "$time - Turn maintinance mode off"
+#docker exec $app_container php /nextcloud/occ maintenance:mode --off
+time=`date +%H:%M:%S`
 echo "$time - Backup process is DONE"
 
